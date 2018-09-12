847dc4582598526fa4cad71467334b5aa2cb973f
dylankirby <dylankirby@outlook.com>
2018-09-12 09:04:40 -0700

    pep8 main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+"""Runs a new instance of the blackjack game"""
 from game import Game
 
+def main():
+    """Starts the game, listens for keyboard interrupt"""
+    game = Game()
+    try:
+        game.new_round()
+    except KeyboardInterrupt:
+        print "Exiting program"
+
 if __name__ == "__main__":
-	game = Game()
-	try:
-		game.new_round()
-	except KeyboardInterrupt:
-		print "Exiting program"
+    main()
